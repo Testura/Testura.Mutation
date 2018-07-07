ea69888310548562ad280a65205cc211761cbd11
Mille Boström <Mille_Bo@hotmail.com>
2018-07-07 17:14:14 +0200

    Add project files.

---
+++ Cama/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using Cama.Views;
+using System.Windows;
+using Prism.Modularity;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+
+namespace Cama
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            var moduleCatalog = (ModuleCatalog)ModuleCatalog;
+            //moduleCatalog.AddModule(typeof(YOUR_MODULE));
+        }
+    }
+}
