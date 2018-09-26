881b73720dc5b34fcb52d8f544d475a2e5d314d0
Mille Boström <Mille_Bo@hotmail.com>
2018-09-26 23:16:08 +0200

    Added back files again

---
+++ src/Cama/Sections/Debug/DebugShellView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Cama.Sections.Debug
+{
+    /// <summary>
+    /// Interaction logic for DebugShellView
+    /// </summary>
+    public partial class DebugShellView : UserControl
+    {
+        public DebugShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Cama/Sections/Debug/DebugShellViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using Cama.Service.Logs;
+using Prism.Mvvm;
+
+namespace Cama.Sections.Debug
+{
+    public class DebugShellViewModel : BindableBase, INotifyPropertyChanged
+    {
+        private readonly LogWatcher _logWatcher;
+
+        public DebugShellViewModel(LogWatcher logWatcher)
+        {
+            _logWatcher = logWatcher;
+            LogText = string.Empty;
+            _logWatcher.NewMessage += LogWatcherOnNewMessage;
+        }
+
+        public string LogText { get; set; }
+
+        private void LogWatcherOnNewMessage(object sender, string s)
+        {
+            LogText += s;
+        }
+    }
+}
