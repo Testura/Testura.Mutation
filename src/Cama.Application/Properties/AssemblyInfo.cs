d546cc11b5b43b8c8c062cd97928f478cb468225
Mille Boström <Mille_Bo@hotmail.com>
2018-11-13 20:40:57 +0100

    Fixed assembly versions

--- src/Cama.Application/Properties/AssemblyInfo.cs
+++ src/Cama.Application/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using Anotar.Log4Net;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Cama.Infrastructure")]
+[assembly: AssemblyTitle("Cama.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP Inc.")]
