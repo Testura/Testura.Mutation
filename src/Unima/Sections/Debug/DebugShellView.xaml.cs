dc2b5fb99a20784f323c64272518b960c14b7937
Mille Boström <mille.bostrom@outlook.com>
2019-05-18 15:19:50 +0200

    Added accidentally ignored file to GIT

---
+++ src/Unima/Sections/Debug/DebugShellView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Unima.Sections.Debug
+{
+    /// <summary>
+    /// Interaction logic for DebugShellView
+    /// </summary>
+    public partial class DebugShellView : UserControl
+    {
+        public DebugShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
