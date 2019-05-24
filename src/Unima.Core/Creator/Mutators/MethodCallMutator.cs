77c9a05caedeecb5cd7dfa88609923fb6da171ab
Mille Boström <mille.bostrom@outlook.com>
2019-05-24 16:10:08 +0200

    Started on new mutation

---
+++ src/Unima.Core/Creator/Mutators/MethodCallMutator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Unima.Core.Creator.Mutators
+{
+    public class MethodCallMutator : Mutator
+    {
+        public override SyntaxNode VisitExpressionStatement(ExpressionStatementSyntax node)
+        {
+            var o = node.ToFullString();
+
+            return base.VisitExpressionStatement(node);
+        }
+    }
+}
---
+++ tests/Unima.Tests/Core/Mutation/Mutators/MethodCallMutatorTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using NUnit.Framework;
+using Unima.Core.Creator.Mutators;
+
+namespace Unima.Tests.Core.Mutation.Mutators
+{
+    [TestFixture]
+    public class MethodCallMutatorTests
+    {
+        [Test]
+        public void GetMutatedDocument_WhenCallingOnAMethodWithoutReturnValue_ShouldReplaceCallWithComment()
+        {
+            var tree = SyntaxFactory.ParseSyntaxTree(@"
+                    class C { 
+                        public void FirstMethod() { Hello(); } 
+                    }");
+            var root = tree.GetRoot();
+
+            var mutator = new MethodCallMutator();
+            var doc = mutator.GetMutatedDocument(root, null);
+
+            Assert.AreEqual("/* Removed this method call */", doc[0].MutationDetails.Mutation.ToString());
+        }
+    }
+}
